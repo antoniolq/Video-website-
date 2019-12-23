da11ad05534878a411ed2e25093e30e3719dac85
antonio <antoniolq@163.com>
2019-12-23 20:16:59 +0800

    update web

--- app/home/views.py
+++ app/home/views.py
@@ -3,7 +3,7 @@ from flask import render_template,redirect,url_for
 
 @home.route("/")
 def index():
-    return render_template("/home/base.html")
+    return render_template("/home/index.html")
     #return "<h1 style='color:green'> this is home </h1>"
 
 @home.route("/login/")
@@ -14,3 +14,26 @@ def login():
 def logout():
     return redirect(url_for("/home.login"))
 
+@home.route("/regist/")
+def regist():
+    return render_template("/home/regist.html")
+
+@home.route("/user/")
+def user():
+    return render_template("/home/user.html")
+
+@home.route("/pwd/")
+def pwd():
+    return render_template("/home/pwd.html")
+
+@home.route("/comments/")
+def comments():
+    return render_template("/home/comments.html")
+
+@home.route("/loginlog/")
+def loginlog():
+    return render_template("/home/loginlog.html")
+
+@home.route("/moviecol/")
+def moviecol():
+    return render_template("/home/moviecol.html")
