6943c37ef09af6eaf0dc52f0a238914d0e3378c7
antonio <antoniolq@163.com>
2019-12-23 16:00:15 +0800

    update

--- app/home/views.py
+++ app/home/views.py
@@ -1,5 +1,16 @@
 from . import home
+from flask import render_template,redirect,url_for
 
 @home.route("/")
 def index():
-    return "<h1 style='color:green'> this is home </h1>"
+    return render_template("/home/index.html")
+    #return "<h1 style='color:green'> this is home </h1>"
+'''
+@home.route("/login/")
+def login():
+    return render_template("/home/login.html")
+
+@home.route("/logout/")
+def logout():
+    return redirect(url_for("/home.login"))
+'''
