2f3fd0a0c2e7bcc7145f99d88e30aa1b575fde7b
qing.liu <qing.liu@ihandysoft.com>
2020-04-04 10:40:37 +0800

    init

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import pymysql
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@127.0.0.1:8889/movie"
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:root@127.0.0.1:3306/movie"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 app.config["SECRET_KEY"] = "543ee8554a184d728183d229c34b8102"
@@ -13,9 +13,9 @@ db = SQLAlchemy(app)
 from app.home import home as home_blueprint
 from app.admin import admin as admin_blueprint
 
-app.register_blueprint(home_blueprint)
+app.register_blueprint(home_blueprint, url_prefix="/home")
 app.register_blueprint(admin_blueprint, url_prefix="/admin")
 
 @app.errorhandler(404)
 def page_not_found(error):
-    return render_template("/home/404.html"),404
+    return render_template("404.html"),404
--- app/admin/views.py
+++ app/admin/views.py
@@ -1,49 +1,55 @@
 from . import admin
 from flask import render_template,redirect,url_for,flash,session,request
-from app.admin.forms import LoginForm,TagForm
+from app.admin.forms import LoginFrom,TagForm
+from app.admin.forms import LoginFrom,TagForm
 from app.models import Admin,Tag
 from functools import wraps
 from app import db
 
-def admin_login_req(f):
-    @wraps(f)
-    def decorated_function(*args,**kwargs):
-        if "admin" not in session:
-            return redirect(url_for("admin.login",next=request.url))
-        return f(*args,**kwargs)
+def admin_login_require(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        if session.get('login_admin', None) is None:
+            # 如果session中未找到该键，则用户需要登录
+            return redirect(url_for('admin.login', next=request.url))
+        return func(*args, **kwargs)
     return decorated_function
 
 @admin.route("/")
-@admin_login_req
+@admin_login_require
 def index():
     return render_template("admin/index.html")
 
-@admin.route("/login/", methods=["GET","POST"])
+@admin.route("/login/", methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
+    print("2")
+    form = LoginFrom()
     if form.validate_on_submit():
-        data = form.data
-        admin = Admin.query.filter_by(name=data["account"]).first()
-        if not admin.check_pwd(data["pwd"]):
-            flash("密码错误！")
-            return redirect(url_for("admin.login"))
-        session["admin"] = data["account"]
-        return redirect(request.args.get("next") or url_for("admin.index"))
-    return render_template("admin/login.html",form=form)
+        # 提交的时候验证表单
+        data = form.data  # 获取表单的数据
+        print(data)
+        login_admin = Admin.query.filter_by(name=data['account']).first()
+        if not login_admin.check_pwd(data['pwd']):
+            # 判断密码错误，然后将错误信息返回，使用flash用于消息闪现
+            flash('密码错误！')
+            return redirect(url_for('admin.login'))
+        # 如果密码正确，session中添加账号记录，然后跳转到request中的next，或者是跳转到后台的首页
+        session['login_admin'] = data['account']
+        return redirect(request.args.get('next') or url_for('admin.index'))
+    return render_template('admin/login.html', form=form)
 
 @admin.route("/logout/")
-@admin_login_req
 def logout():
-    session.pop("admin",None)
+    session.pop('login_admin', None)  # 删除session中的登录账号
     return redirect(url_for("admin.login"))
 
 @admin.route("/pwd/")
-@admin_login_req
+@admin_login_require
 def pwd():
     return render_template("admin/pwd.html")
 
 @admin.route("/tag/add/",methods=["GET","POST"])
-@admin_login_req
+@admin_login_require
 #TODO
 def tag_add():
     form = TagForm()
@@ -63,7 +69,7 @@ def tag_add():
     return render_template("/admin/tag_add.html",form=form)
 
 @admin.route("/tag/list/<int:page>/", methods=["GET"])
-@admin_login_req
+@admin_login_require
 def tag_list(page=None):
     if page is None:
         page = 1
@@ -73,86 +79,86 @@ def tag_list(page=None):
     return render_template("/admin/tag_list.html",page_data=page_data)
 
 @admin.route("/movie/add/")
-@admin_login_req
+@admin_login_require
 def movie_add():
     return render_template("/admin/movie_add.html")
 
 @admin.route("/movie/list/")
-@admin_login_req
+@admin_login_require
 def movie_list():
     return render_template("/admin/movie_list.html")
 
 @admin.route("/preview/add/")
-@admin_login_req
+@admin_login_require
 def preview_add():
     return render_template("/admin/preview_add.html")
 
 @admin.route("/preview/list/")
-@admin_login_req
+@admin_login_require
 def preview_list():
     return render_template("/admin/preview_list.html")
 
 @admin.route("/user/list/")
-@admin_login_req
+@admin_login_require
 def user_list():
     return render_template("/admin/user_list.html")
 
 @admin.route("/user/view/")
-@admin_login_req
+@admin_login_require
 def user_view():
     return render_template("/admin/user_view.html")
 
 @admin.route("/comment/list/")
-@admin_login_req
+@admin_login_require
 def comment_list():
     return render_template("/admin/comment_list.html")
 
 @admin.route("/moviecol/list/")
-@admin_login_req
+@admin_login_require
 def moviecol_list():
     return render_template("/admin/moviecol_list.html")
 
 @admin.route("/oplog/list/")
-@admin_login_req
+@admin_login_require
 def oplog_list():
     return render_template("/admin/oplog_list.html")
 
 @admin.route("/adminloginlog/list/")
-@admin_login_req
+@admin_login_require
 def adminloginlog_list():
     return render_template("/admin/adminloginlog_list.html")
 
 @admin.route("/userloginlog/list/")
-@admin_login_req
+@admin_login_require
 def userloginlog_list():
     return render_template("/admin/userloginlog_list.html")
 
 @admin.route("/role/add/")
-@admin_login_req
+@admin_login_require
 def role_add():
     return render_template("/admin/role_add.html")
 
 @admin.route("/role/list/")
-@admin_login_req
+@admin_login_require
 def role_list():
     return render_template("/admin/role_list.html")
 
 @admin.route("/auth/add/")
-@admin_login_req
+@admin_login_require
 def auth_add():
     return render_template("/admin/auth_add.html")
 
 @admin.route("/auth/list/")
-@admin_login_req
+@admin_login_require
 def auth_list():
     return render_template("/admin/auth_list.html")
 
 @admin.route("/admin/add/")
-@admin_login_req
+@admin_login_require
 def admin_add():
     return render_template("/admin/admin_add.html")
 
 @admin.route("/admin/list/")
-@admin_login_req
+@admin_login_require
 def admin_list():
     return render_template("/admin/admin_list.html")
